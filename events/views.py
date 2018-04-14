8b6d13986b04f35b964d08691ef0adefbcca5939
Cmoyano <cristianmoyano.mza@gmail.com>
2018-04-14 11:55:15 -0300

    refactor: get events from API

--- events/views.py
+++ events/views.py
@@ -2,20 +2,24 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.utils import timezone
-from django.shortcuts import redirect
-from social_django.models import UserSocialAuth
-from django.http.response import HttpResponse
-import requests
-from .models import Evento,Asistente_Evento
+from eventbrite import Eventbrite
+
 
 def home(request):
-    return render(request, 'events/home.html',{})
+    return render(request, 'events/home.html', {})
+
 
 def all(request):
-    events = Evento.objects.all()
-    return render(request, 'events/eventos.html',{'eventos': events})
-
-def go(request,pk):
-        events = Evento.objects.filter(pk=pk)
-        return render(request, 'events/evento.html',{'evento': events})
+    # events = Evento.objects.all()
+    access_token = request.user.social_auth.get(
+        provider=request.session.get('social_auth_last_login_backend')
+    ).access_token
+    eventbrite = Eventbrite(access_token)
+    events = [
+        event
+        # Status : live, draft, canceled, started, ended, all
+        for event in eventbrite.get(
+            '/users/me/events/?status=live,draft'
+        )['events']
+    ]
+    return render(request, 'events/eventos.html', {'events': events})
